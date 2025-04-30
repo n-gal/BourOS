c78feda3da84c90814bf0c399084000f831a730f
n-gal <147702076+n-gal@users.noreply.github.com>
2025-04-30 23:25:19 +0200

    Added print and print hex functions

--- ASM/BootSector.asm
+++ ASM/BootSector.asm
@@ -5,30 +5,29 @@
 ; Memory offset from where the boot sector is located
 [org 0x7c00]
 
-mov ah, 0x0e ; tty mode
+; The main routine makes sure the parameters are ready and then calls the function
+mov bx, TITLE
+call print
 
-mov al, 'B'
-int 0x10
+call print_nl
+mov bx, VERSION
+call print
 
-mov al, 'o'
-int 0x10
+; that's it! we can hang now
+jmp $
 
-mov al, 'u'
-int 0x10
+; remember to include subroutines below the hang
+%include "ASM/BootSectorPrint.asm"
+%include "ASM/BootSectorPrintHex.asm"
 
-mov al, 'r'
-int 0x10
 
-mov al, 'O'
-int 0x10
+; data
+TITLE:
+    db 'BourOS', 0
 
-mov al, 'S'
-int 0x10
-
-jmp $ ; jump to current address = infinite loop
+VERSION:
+    db 'Ver 1.0', 0
 
 ; padding and magic number
-; these are compile time directives, it makes the assembler fill the rest of the file with 0s until it reaches 510 bytes
-; this means that the file has to be 512 bytes long, 0xaa55 is read by the BIOS to know that it is a boot sector
-times 510 - ($-$$) db 0
-dw 0xaa55 
+times 510-($-$$) db 0
+dw 0xaa55
