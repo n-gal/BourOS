c7a7fa3df26c3f4fecf0709bda05ef14816c74e5
n-gal <147702076+n-gal@users.noreply.github.com>
2025-05-02 22:13:43 +0200

    removed superfluous bat files
    see i use fancy words

--- Compile.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-nasm -f bin ASM/BootSector.asm -o ASM/BootSector.bin
-echo Build complete: BootSector.bin generated.
-pause
-REM 
--- Run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-qemu-system-x86_64 ASM/BootSector.bin
-pause
-REM 
