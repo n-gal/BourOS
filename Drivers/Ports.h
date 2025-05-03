f7b1f67501e89b4a54f3243722c542bb0920ca84
n-gal <147702076+n-gal@users.noreply.github.com>
2025-05-03 11:37:54 +0200

    Basic port IO

--- Drivers/Ports.h
+++ Drivers/Ports.h
@@ -1,4 +1,4 @@
-unsigned char port_byte_in (unsigned short port);
-void port_byte_out (unsigned short port, unsigned char data);
-unsigned short port_word_in (unsigned short port);
-void port_word_out (unsigned short port, unsigned short data);
+unsigned char PortByteIn (unsigned short port);
+void PortByteOut (unsigned short port, unsigned char data);
+unsigned short PortWordIn (unsigned short port);
+void PortWordOut (unsigned short port, unsigned short data);
